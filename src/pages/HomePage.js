a77d1b5ede725ad774187edcce6fedb6bf0f0417
Fuzail Misarwala <fuzail.misarwala@cerner.com>
2019-11-25 22:13:47 -0600

    Add twitter embed on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import TabPanel from '../components/TabPanel';
+import { TwitterTimelineEmbed, TwitterFollowButton } from 'react-twitter-embed';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const HomePage = ({ value }) => {
+  const classes = useStyles();
   return (
     <TabPanel index={0}>
-      Home
+      <Container maxWidth='lg'>
+        <Grid container spacing={3}>
+          <Grid item xs={9}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={3}>
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="zenithonlinef1"
+            placeholder="Loading"
+            options={{height: 500}}
+          />
+          <TwitterFollowButton screenName="zenithonlineF1" options={{size: 'large'}} />
+          </Grid>
+        </Grid>
+      </Container>
     </TabPanel>
   )
 };
