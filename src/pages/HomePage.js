a0875632fccf00498c81cd2305110945d8c13539
Fuzail Misarwala <fuzail.misarwala@cerner.com>
2019-11-26 11:50:09 -0600

    Clean up homepage code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -23,21 +23,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const images = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
-  },
-];
-
 const HomePage = ({ value }) => {
   const classes = useStyles();
   return (
