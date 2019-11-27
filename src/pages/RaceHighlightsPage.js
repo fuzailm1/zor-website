d85f2b4465bab2aa046e2a3511fdc7a0b560de5d
Fuzail Misarwala <fuzail.misarwala@cerner.com>
2019-11-27 14:33:43 -0600

    responsive iframe highlights

--- src/pages/RaceHighlightsPage.js
+++ src/pages/RaceHighlightsPage.js
@@ -12,28 +12,70 @@ const RaceHighlightsPage = () => {
             <Typography className={classes.paper}>
                 <h1>Race Highlights and Streams</h1>
             </Typography>
-            <Typography className={classes.paper}>
-                <h4>
-                    ZOR Season 2 - Belgium
-                </h4>
+            <Typography className={classes.paper} style={{paddingTop: "50px"}}>
+                <h3>
+                    ZOR Season 2 Social - Mexico
+                </h3>
             </Typography>
             <div style={{
-                    width: "100%", 
-                    maxWidth: "100%", 
-                    display: "flex", 
-                    flexWrap: "wrap", 
-                    justifyContent: "center", 
-                    alignItems: "center"
+                     position: "relative",
+                     overflow: "hidden",
+                     paddingTop: "56.25%",
+                }}>
+                <iframe
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        border: 0,
+                    }}
+                    src={`https://www.youtube.com/embed/0v0VmQoneSA`}
+                    frameBorder="10"
+                />
+            </div>
+            <Typography className={classes.paper} style={{paddingTop: "50px"}}>
+                <h3>
+                    ZOR Season 2 - Japan
+                </h3>
+            </Typography>
+            <div style={{
+                     position: "relative",
+                     overflow: "hidden",
+                     paddingTop: "56.25%",
+                }}>
+                <iframe
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        border: 0,
+                    }}
+                    src={`https://www.youtube.com/embed/icY1tbK9C7g`}
+                    frameBorder="10"
+                />
+            </div>
+            <Typography className={classes.paper} style={{paddingTop: "50px"}}>
+                <h3>
+                    ZOR Season 2 - Belgium
+                </h3>
+            </Typography>
+            <div style={{
+                     position: "relative",
+                     overflow: "hidden",
+                     paddingTop: "56.25%",
                 }}>
                     <iframe
                         style={{
-                            position: "relative",
+                            position: "absolute",
                             top: 0,
                             left: 0,
-                            width: "60vw",
-                            height: "68vh",
-                            maxWidth: "100%",
-                            maxHeight: "80%"
+                            width: "100%",
+                            height: "100%",
+                            border: 0,
                         }}
                         src={`https://www.youtube.com/embed/EJUfw9upikI`}
                         frameBorder="10"
